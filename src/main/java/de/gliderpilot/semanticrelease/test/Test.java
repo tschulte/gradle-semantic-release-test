0cf06641e24c2dca885d9c0dafc346ab630669b2
Tobias Schulte <tobias.schulte@gliderpilot.de>
2016-08-11 12:53:10 +0200

    feat: add service `Test` with method helloWorld()

---
+++ src/main/java/de/gliderpilot/semanticrelease/test/Test.java
@@ -0,0 +1,13 @@
+package de.gliderpilot.semanticrelease.test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(helloWorld());
+    }
+
+    public static String helloWorld() {
+        return "Hello World";
+    }
+
+}
