ccf09e1ae75d1065dddfaa133ae60c4e0dcc8886
Tobias Schulte <Tobias.Schulte@kratzer-automation.com>
2017-03-17 10:36:33 +0100

    feat(main): remove deprecated methods helloWorld() and hello()
    BREAKING CHANGE: remove deprecated methods helloWorld() and hello()
    use greet("Hello", "World") instead

--- src/main/java/de/gliderpilot/semanticrelease/test/Test.java
+++ src/main/java/de/gliderpilot/semanticrelease/test/Test.java
@@ -6,22 +6,6 @@ public class Test {
         System.out.println(greet("Hello", "World"));
     }
 
-    /**
-     * @deprecated Use greet("Hello", "World") instead.
-     */
-    @Deprecated
-    public static String helloWorld() {
-        return hello("World");
-    }
-
-    /**
-     * @deprecated Use greet("Hello", who) instead.
-     */
-    @Deprecated
-    public static String hello(String who) {
-        return greet("Hello", who);
-    }
-
     public static String greet(String greeting, String who) {
         return greeting + " " + who;
     }
